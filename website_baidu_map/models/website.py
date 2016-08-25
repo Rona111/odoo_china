7914506c3071c8fa1944bd7283ce3ab160f96214
wangting <39181819@qq.com>
2016-08-25 08:56:49 +0800

    [imp]

--- website_baidu_map/models/website.py
+++ website_baidu_map/models/website.py
@@ -6,7 +6,4 @@ from openerp import models, fields, api
 class Website(models.Model):
     _inherit = "website"
 
-    social_sina = fields.Char('Sina Account')
-    social_weixin_qrcode = fields.Char('Weixin QR Code', help="微信公众号二维码图片在服务器上的路径。")
-
     baidu_map_AK = fields.Char(help="百度地图的访问授权。可以在这里申请 http://lbsyun.baidu.com/apiconsole/key")
